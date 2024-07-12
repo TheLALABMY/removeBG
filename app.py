e4c6bf3ac56c31a6fd5d2d05c5e5f18734b73183
LA LAB <thelalabmy@gmail.com>
2024-07-12 09:04:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -110,4 +110,5 @@ def uploaded_file(filename):
 if __name__ == '__main__':
     if not os.path.exists(app.config['UPLOAD_FOLDER']):
         os.makedirs(app.config['UPLOAD_FOLDER'])
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Use PORT environment variable or default to 5000
+    app.run(host="0.0.0.0", port=port)
